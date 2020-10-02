0a62a704cf7363edb51e4260c6575bc3f41939f5
Joise MP <joise1124@gmail.com>
2020-10-02 20:53:59 +0530

    installed PyPDF2 package and modified the file to an pdf reader. Need to make some changes for the smooth functioning

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from gtts import gTTS
 import os  # Imported the required module for text to speech
+import PyPDF2
+
+file_name = input("Enter the file name : ")
+book = open(file_name, 'rb')
+pdf_reader = PyPDF2.PdfFileReader(book)
+pages = pdf_reader.numPages
+page_no = int(input("Enter a page no to convert it's text to speech : "))
+
+page = pdf_reader.getPage(page_no)
+text = page.extractText()
 
 # The text that you want to convert to audio
-mytext = input("Enter the text that you want to convert to speech : ")
+mytext = text
 
 # Language in which you want to convert
-language = 'ml'
+language = 'en'
 
 # Passing the text and language to the engine,
 # here I have marked slow=False. Which tells
@@ -13,7 +23,8 @@ language = 'ml'
 # have a high speed
 myobj = gTTS(text=mytext, lang=language, slow=False)
 
-converted_file_name = str(input("Enter your new file name with extension : "))
+new_file_name = input("Enter your converted file name : ")
+converted_file_name = new_file_name + ".mp3"
 myobj.save(converted_file_name)  # Saving the converted audio in a mp3 file in the file name you entered
 
 # Playing the converted file
