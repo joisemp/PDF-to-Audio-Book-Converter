0c059e1d02a09f3014ea6656b8e3aa391f2d192d
Joise MP <joise1124@gmail.com>
2020-10-02 16:53:04 +0530

    finished creating text to speech app

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 from gtts import gTTS
 import os  # Imported the required module for text to speech
 
+# The text that you want to convert to audio
+mytext = input("Enter the text that you want to convert to speech : ")
+
+# Language in which you want to convert
+language = 'ml'
+
+# Passing the text and language to the engine,
+# here I have marked slow=False. Which tells
+# the module that the converted audio should
+# have a high speed
+myobj = gTTS(text=mytext, lang=language, slow=False)
+
+converted_file_name = str(input("Enter your new file name with extension : "))
+myobj.save(converted_file_name)  # Saving the converted audio in a mp3 file in the file name you entered
+
+# Playing the converted file
+os.system(converted_file_name)
