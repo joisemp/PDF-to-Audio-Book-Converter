ea8dd2236db6f6f7ab450db708dbe14597a76bb6
Joise MP <joise1124@gmail.com>
2020-10-02 23:14:37 +0530

    reformated the code and added some documentation

--- main.py
+++ main.py
@@ -3,29 +3,22 @@ import os  # Imported the required module for text to speech
 import PyPDF2
 
 file_name = input("Enter the file name : ")
-book = open(file_name, 'rb')
+book = open(file_name, 'rb')  # converting the file to binary format
 pdf_reader = PyPDF2.PdfFileReader(book)
 pages = pdf_reader.numPages
 page_no = int(input("Enter a page no to convert it's text to speech : "))
 
-page = pdf_reader.getPage(page_no)
-text = page.extractText()
+page = pdf_reader.getPage(page_no)  # storing the entered page no to the variable page
+text = page.extractText()  # extracting text from the page
 
-# The text that you want to convert to audio
-mytext = text
+my_text = text  # assigning the extracted text to the variable my_text
 
-# Language in which you want to convert
 language = 'en'
 
-# Passing the text and language to the engine,
-# here I have marked slow=False. Which tells
-# the module that the converted audio should
-# have a high speed
-myobj = gTTS(text=mytext, lang=language, slow=False)
+my_obj = gTTS(text=my_text, lang=language, slow=False)
 
 new_file_name = input("Enter your converted file name : ")
 converted_file_name = new_file_name + ".mp3"
-myobj.save(converted_file_name)  # Saving the converted audio in a mp3 file in the file name you entered
+my_obj.save(converted_file_name)  # Saving the converted audio in a mp3 file in the file name you entered
 
-# Playing the converted file
-os.system(converted_file_name)
+os.system(converted_file_name)  # Playing the converted file
